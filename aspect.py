47034491e2630be457cf751fbc4eec59c932fac5
thegreathippo <thegreathippo@gmail.com>
2016-11-29 15:20:49 -0500

    Update aspect.py

--- aspect.py
+++ aspect.py
@@ -8,6 +8,7 @@ class Components:
   
   def __init__(self, *components):
     self.__dict__.update({k:{} for k in components})
+    self._processes = []
 
   def add_aspect(self, name, *components):
     self.__dict__[name] = Aspect(**{c:self.__dict__[c] for c in components})
@@ -16,19 +17,17 @@ class Components:
 class Aspect:
   
   def __init__(self, **components):
-    self._components = components.values()
-    self._views = []
-    for d in components.values():
-      self._views.append(d.keys())
-    super().__init__()
+    self.__dict__.update(components)
 
   def get_uids(self):
-    return set.intersection(*[set(s) for s in self._views])
+    return list(set.intersection(*[set(s) for s in self.__dict__.values()]))
 
   def clear_uid(self, uid):
-    for d in self._components:
+    for d in self.__dict__.values():
       if uid in d:
         del d[uid]
+
+
 
 
 components = Components("x", "y", "vx", "vy")
@@ -40,7 +39,24 @@ components.x[wall], components.y[wall] = 0, 0
 
 
 components.add_aspect("Physics", "x", "y", "vx", "vy")  
+components.add_aspect("Position", "x", "y")
 
-print(components.Physics.get_uids())
+
+print("Physics: " + str(components.Physics.get_uids()))
+print("Position: " + str(components.Position.get_uids()))
+print("Removing vx from Player")
+del components.vx[player]
+print("Physics: " + str(components.Physics.get_uids()))
+print("Position: " + str(components.Position.get_uids()))
+print("Removing everything from Player")
 components.Physics.clear_uid(player)
-print(components.Physics.get_uids())
+print("Physics: " + str(components.Physics.get_uids()))
+print("Position: " + str(components.Position.get_uids()))
+components.x[player]=1
+components.y[player]=0
+components.vx[player]=0
+components.vy[player]=0
+print("Physics: " + str(components.Physics.get_uids()))
+print("Position: " + str(components.Position.get_uids()))
+
+
