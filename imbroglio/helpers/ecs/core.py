e9de987cc13b5dcd56a5159d660acc325a064a47
thegreathippo <thegreathippo@gmail.com>
2016-12-03 03:57:27 -0500

    Update core.py

--- imbroglio/helpers/ecs/core.py
+++ imbroglio/helpers/ecs/core.py
@@ -1,10 +1,6 @@
 # Entity-Component architecture based loosely on JAForbes' idea:
 # https://gist.github.com/JAForbes/99c15c0995b87a22b95a
 
-world = 0
-player = 1
-monster = 2
-
 
 class AspectType(type):
   """Metaclass for Aspects."""
@@ -28,6 +24,10 @@ class BaseAspect(dict, metaclass=AspectType):
     if not set(self.domain).issubset(self.root.keys()):
       # write a more descriptive exception later
       raise Exception("{0} instanced with invalid domain ({1})".format(self, self.domain))
+    self.startup()
+
+  def startup(self):
+    pass
 
   def setup(self):
     pass
@@ -39,7 +39,7 @@ class BaseAspect(dict, metaclass=AspectType):
     self.teardown()
 
   def run(self, uid):
-    print(str(self) + ": " + str(uid))
+    pass
 
   def teardown(self):
     pass
@@ -82,20 +82,3 @@ class _Entities(dict):
       return item()
     return item
 
-component = Components("x", "y")
-component["x"][player] = 1
-component["y"][player] = 2
-component["x"][monster] = 0
-component["y"][monster] = 4
-
-
-class Physics(component.Aspect):
-  domain = {"x", "y"}
-
-
-class Physics2(component.Aspect):
-  domain = {"x", "y"}
-  priority = -1
-
-
-component.run()
