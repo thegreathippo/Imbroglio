a08abf2820dbf001cc59b319f1599e238df47cea
thegreathippo <thegreathippo@gmail.com>
2016-12-03 22:05:10 -0500

    Update test_core.py

--- tests/helpers/parser/test_core.py
+++ tests/helpers/parser/test_core.py
@@ -1,5 +1,5 @@
 import unittest
-from ...helpers.parser import Parser
+from ..helpers.parser import Parser
 
 # Simple test.
 test1_inf = "(12 - 3 ) / 3^2 + 2 * 3"
@@ -18,60 +18,60 @@ test4_inf = "max((2 + 3) / 5 * min(5, 2 + 2), 10)"
 test4_ans = 10
 
 # Everything test.
-test5_inf = "((5 + min(max(self.x, 9), 11)) / 5)^2"
+test5_inf = "((5 + min(max(component.x.entity, 9), 11)) / 5)^2"
 test5_ans = 9
 
 class CoreParserTest(unittest.TestCase):
   def setUp(self):
     self.tester = 0
-    self.components = {"x" : {self.tester : 10}}
-    self.parser = Parser(self.components)
+    self.component = {"x" : {self.tester : 10}}
+    self.parser = Parser(component=self.component, entity=self.tester)
 
   def test_parser_returns_callable(self):
     """Test to ensure the parser returns a callable function."""
-    func = self.parser("", 0)
+    func = self.parser("")
     self.assertTrue(callable(func))
 
   def test_simple_answer(self):
     """Test to ensure the parser's function resolves simple 
     expressions correctly.
     """
-    answer = self.parser(test1_inf, self.tester)
+    answer = self.parser(test1_inf)
     self.assertEqual(test1_ans, answer())
 
   def test_complex_answer(self):
     """Test to ensure the parser's function resolves complex 
     expressions correctly.
     """
-    answer = self.parser(test2_inf, self.tester)
+    answer = self.parser(test2_inf)
     self.assertEqual(test2_ans, answer())
 
   def test_float_answer(self):
     """Test to ensure the parser's function resolves expressions
     with floats correctly.
     """
-    answer = self.parser(test3_inf, self.tester)
+    answer = self.parser(test3_inf)
     self.assertAlmostEqual(test3_ans, answer())
 
   def test_function_answer(self):
     """Test to ensure the parser's function resolves expressions
     with functions correctly.
     """
-    answer = self.parser(test4_inf, self.tester)
+    answer = self.parser(test4_inf)
     self.assertEqual(test4_ans, answer())
 
   def test_can_access_component_value(self):
     """Test to ensure the parser's function can access component
     values correctly.
     """
-    answer = self.parser("self.x", self.tester)
+    answer = self.parser("component.x.entity")
     self.assertEqual(10, answer())
 
   def test_everything(self):
     """Test to ensure the parser's function can resolve an 
     expression that contains all the features described above.
     """
-    answer = self.parser(test5_inf, self.tester)
+    answer = self.parser(test5_inf)
     self.assertEqual(test5_ans, answer())
 
 unittest.main()
