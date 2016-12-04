9d574ae4aa174a039b82ec8c9f10d6ff9f6ef9c3
thegreathippo <thegreathippo@gmail.com>
2016-12-04 04:34:01 -0500

    Update core.py

--- imbroglio/helpers/reader/core.py
+++ imbroglio/helpers/reader/core.py
@@ -1,40 +1,19 @@
-from ecs import Components
+from .ecs import Components
 import re
 
 def load_components(path):
-  components = dict()
-  file = open(path, "r")
-  for line in file.readlines():
-    if re.match(r'\n', line): # new line
-      continue
-    comp_to_val = line.replace(" ", "").replace("\n", "").split("=")
-    if len(comp_to_val) != 2:
-      continue
-    key, val = comp_to_val
-    try:
-      val = int(val)
-    except ValueError:
-      pass
-    components[key] = val
-  return components
-
-data = load_components("components.dat")
-components = Components(**data)
-abilities = "strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"
-components.set_components(0, *abilities)
-mods = "str_bonus", "dex_bonus", "con_bonus", "int_bonus", "wis_bonus", "cha_bonus"
-components.set_components(0, *mods)
-components.set_components(0, "all_bonus")
-print(components["strength"][0])
-print(components["dexterity"][0])
-print(components["constitution"][0])
-print(components["intelligence"][0])
-print(components["wisdom"][0])
-print(components["charisma"][0])
-print(components["str_bonus"][0])
-print(components["dex_bonus"][0])
-print(components["con_bonus"][0])
-print(components["int_bonus"][0])
-print(components["wis_bonus"][0])
-print(components["cha_bonus"][0])
-print(components["all_bonus"][0])
+  data = dict()
+  with open(path, "r") as file:
+    for line in file.readlines():
+      if re.match(r'\n', line): # new line
+        continue
+      comp_to_val = line.replace(" ", "").replace("\n", "").split("=")
+      if len(comp_to_val) != 2:
+        continue
+      key, val = comp_to_val
+      try:
+        val = int(val)
+      except ValueError:
+        pass
+      data[key] = val
+  return Components(**data)
