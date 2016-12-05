7eaf976654d093e054b5d75b1101f20d135dc254
thegreathippo <thegreathippo@gmail.com>
2016-12-05 01:45:41 -0500

    Update entities.py

--- imbroglio/helpers/ecs/entities.py
+++ imbroglio/helpers/ecs/entities.py
@@ -1,12 +1,10 @@
 """Entity module for Imbroglio's ecs (Entity Component architecture).
-
 This module provides access to the Entities and EntityHandler classes; 
 Entities inherits from Python 3.x's dictionary, and effectively just 
 has a unique __getitem__ method (if the returning value is callable, it
 calls it and returns the result). EntityHandler provides a clean 
 interface to retrieve an entity's components via attribute access 
 (entity.x == components["x"][entity]).
-
 TODO:
   * None, as of now (?)
 """
@@ -19,10 +17,26 @@ class Entities(dict):
     return item
 
 
-class EntityHandler:
-  def __init__(self, uid, _dict):
-    self._uid = uid
-    self._dict = _dict
+class BaseEntity:
+  root = None
+
+  def __init__(self, uid):
+    super().__setattr__("_uid", uid)
+
+  def get_uid(self):
+    return self._uid
 
   def __getattr__(self, attr):
-    return self._dict[attr][self._uid]
+    return self.root[attr][self._uid]
+
+  def __setattr__(self, attr, value):
+    self.root[attr][self._uid] = value
+  
+  def __hash__(self):
+    return hash((self.get_uid(), type(self)))
+  
+  def __eq__(self, other):
+    try:
+      return ((other.get_uid(), type(other)) == (self.get_uid(), type(self)))
+    except AttributeError:
+      return False
