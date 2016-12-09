cb0b428cef61678483986e6b6f53e450378d5cf7
thegreathippo <thegreathippo@gmail.com>
2016-12-09 16:18:08 -0500

    Update entities.py

--- toy/entities.py
+++ toy/entities.py
@@ -1,3 +1,7 @@
+"""
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+"""
+
 class ComponentDict(dict):
   def __getitem__(self, key):
     if hasattr(key, "get_eid"):
@@ -8,12 +12,14 @@ class ComponentDict(dict):
 class BaseEntity:
   root = None
 
-  def __init__(self, *args, **kwargs):
-    if not args:
+  def __init__(self, _eid=None, **kwargs):
+    if _eid is None:
       eid = self.root.eid
       self.root.eid += 1
     else:
-      eid = args[0]
+      if hasattr(_eid, "get_eid"):
+        _eid = _eid.get_eid()
+      eid = _eid
     super().__setattr__("_eid", eid)
     for key in kwargs:
       if kwargs[key] is None:
