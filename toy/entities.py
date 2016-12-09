b06056db03de04434c03e4c2729fe02ba607bcc5
thegreathippo <thegreathippo@gmail.com>
2016-12-08 20:59:16 -0500

    Update entities.py

--- toy/entities.py
+++ toy/entities.py
@@ -1,12 +1,19 @@
-from modifiers import Modifiers
+class ComponentDict(dict):
+  def __getitem__(self, key):
+    if hasattr(key, "get_eid"):
+      key = key.get_eid()
+    return super().__getitem__(key)
+
 
 class BaseEntity:
   root = None
 
-  def __init__(self, eid=None, **kwargs):
-    if eid is None:
+  def __init__(self, *args, **kwargs):
+    if not args:
       eid = self.root.eid
       self.root.eid += 1
+    else:
+      eid = args[0]
     super().__setattr__("_eid", eid)
     for key in kwargs:
       if kwargs[key] is None:
@@ -24,5 +31,14 @@ class BaseEntity:
     return value
 
   def __setattr__(self, attr, value):
-    self.root[attr][self._eid] = Modifiers(self, value)
+    if self._eid in self.root[attr]:
+      self.root[attr][self._eid].base = value
+    else:
+      self.root[attr][self._eid] = self.root.Value(self._eid, value)
+
+  def __delattr__(self, attr):
+    del self.root[attr][self._eid]
+  
+  def __getitem__(self, key):
+    return self.root[key][self._eid]
 
