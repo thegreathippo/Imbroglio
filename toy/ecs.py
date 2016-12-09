7448d2a3ade22109b9183cebb10e3fae8fa07f75
thegreathippo <thegreathippo@gmail.com>
2016-12-09 16:17:47 -0500

    Update ecs.py

--- toy/ecs.py
+++ toy/ecs.py
@@ -1,6 +1,11 @@
+"""
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+"""
+
 from entities import ComponentDict, BaseEntity
 from modifiers import BaseModifierType
 from values import BaseValue
+from processes import BaseProcess
 
 def empty():
   pass
@@ -11,8 +16,9 @@ class System(dict):
     self.default = dict(kwargs)
     self._processes = dict()
     self._process_queue = list()
-    self._modtypes = dict()
+    self.modtypes = dict()
     self.eid = 0
+    self._running = False
     
     class Value(BaseValue):
       root = self
@@ -22,10 +28,14 @@ class System(dict):
 
     class ModType(BaseModifierType):
       root = self
+    
+    class Process(BaseProcess):
+      root = self
 
     self.Value = Value
     self.Entity = Entity
     self.ModType = ModType
+    self.Process = Process
 
     for key in kwargs:
       self[key] = ComponentDict()
@@ -36,11 +46,35 @@ class System(dict):
     if not domain:
       domain = set()
 
-    self._processes[process] = {"domain":domain, "startup":startup, 
+    # add exception here for invalid domains? IE, domains with components that
+    # don't exist?
+    domain_dict = {k:v for k, v in self.items() if k in domain}
+    self._processes[process] = {"domain":domain_dict, "startup":startup, 
                                 "setup":setup, "teardown":teardown,
                                 "shutdown":shutdown, "priority":priority}
     self._process_queue.append(process)
     self._process_queue.sort(key=lambda x: self._processes[x]["priority"])
   
   def register_modtype(self, modtype, name):
-    self._modtypes[name] = modtype
+    self.modtypes[name] = modtype
+  
+  def step(self):
+    if self._running is False:
+      for process in self._process_queue:
+        self._processes[process]["startup"]()
+      self._running = True
+    for process in self._process_queue:
+      self._processes[process]["setup"]()
+      domain = self._processes[process]["domain"]
+      if domain:
+        eids = list(set.intersection(*[set(s) for s in domain.values()]))
+      else:
+        eids = list()
+      for eid in eids:
+        process(self.Entity(eid))
+      self._processes[process]["teardown"]()
+
+
+  def quit(self):
+    for process in self._process_queue:
+      self._processes[process]["shutdown"]()
