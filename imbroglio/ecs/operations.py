205fcd812cf1041ffa3f424892134c3e43be71a1
thegreathippo <thegreathippo@gmail.com>
2016-12-13 09:17:58 -0500

    Update operations.py

--- imbroglio/ecs/operations.py
+++ imbroglio/ecs/operations.py
@@ -9,15 +9,13 @@ RIGHT = R
 LEFT = L
 
 def add(val1, val2):
-	return val1 + val2
+  return val1 + val2
 
 def subtract(val1, val2):
-	return val1 - val2
+  return val1 - val2
 
 def divide(val1, val2):
-	answer = val1 / val2
-	if int(answer) == answer:
-		return int(answer)
+	answer = val1 // val2
 	return answer
 
 def multiply(val1, val2):
